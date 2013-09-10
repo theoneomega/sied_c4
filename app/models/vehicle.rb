2808fe1f1b5a76f43ef894885ef7b222bafd5c8e
luis m <luism.x89@gmail.com>
2013-09-10 11:29:00 -0600

    mas internacionalizacion

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -1,6 +1,6 @@
 class Vehicle < ActiveRecord::Base
   attr_accessible :brand, :color, :details, :id, :line, :model, :person_id, :serial_number, :status, :stolen, :plate
-  attr_accessible :fulldescription
+  attr_accessible :fulldescription, :people_attributes
   belongs_to :person
   has_many :criminal_vehicle, :dependent => :destroy
   
