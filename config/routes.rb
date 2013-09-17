979203f273ff8867169a29dc3c10a8fc6ed67772
Joel I. Melendez <theoneomega@gmail.com>
2013-09-17 16:27:59 -0600

    orden decendente eventos por prioridad

--- config/routes.rb
+++ config/routes.rb
@@ -116,7 +116,7 @@ Sied::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'welcome#index'
+   root :to => 'events#index'
 
   # See how all your routes lay out with "rake routes"
 
